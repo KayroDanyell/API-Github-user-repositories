ffd841fed84684d218545b7a08ada42760651ded
KayroDanyell <68576494+KayroDanyell@users.noreply.github.com>
2020-11-06 17:30:00 -0300

    Add files via upload

---
+++ API Github/API.js
@@ -0,0 +1,31 @@
+var user = document.getElementById('usuario')
+var envio = document.getElementById('enviar')
+var lista = document.getElementById('list')
+
+
+var usuario = user.value
+var link = 'https://api.github.com/users/'+ usuario +'/repos'
+console.log(link)
+
+
+function requis(){
+    var usuario = user.value
+    var link = 'https://api.github.com/users/'+ usuario +'/repos'
+    
+    axios.get(link)
+        .then(function(response){
+            var resposta = response.data
+            console.log(resposta)
+            for(repo of resposta){
+                var repoElement = document.createElement('li')
+                var repoText = document.createTextNode(repo['name'])
+
+                repoElement.appendChild(repoText)
+                lista.appendChild(repoElement)
+                
+            }
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+}
